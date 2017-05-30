4e7cff0fb0dd287eb46e4f049050530376b01473
DAO HIEU <15520219@gm.uit.edu.vn>
2017-05-30 20:06:05 +0700

    FINAL PROJECT 1.1

--- TỆP MÃ NGUỒN/FILE SOURCE/QL_AMNHAC_H2B/PROJECT/GUI/NGUOIDUNG/playlist_chuaco.cs
+++ TỆP MÃ NGUỒN/FILE SOURCE/QL_AMNHAC_H2B/PROJECT/GUI/NGUOIDUNG/playlist_chuaco.cs
@@ -31,20 +31,24 @@ namespace PROJECT.GUI.NGUOIDUNG
             textBox2.Text = "";
             //phan danh sach nhac
             dataGridView1.DataSource = lg.get_BAIHAT().ToList();
-            dataGridView1.Columns[0].Visible = false;
             dataGridView1.Columns[1].Visible = false;
             dataGridView1.Columns[2].Visible = false;
             dataGridView1.Columns[3].Visible = false;
-            dataGridView1.Columns[5].Visible = false;
-            dataGridView1.Columns[7].Visible = false;
-            dataGridView1.Columns[8].Visible = false;
+            //dataGridView1.Columns[0].Visible = false;
+            //dataGridView1.Columns[1].Visible = false;
+            //dataGridView1.Columns[2].Visible = false;
+            //dataGridView1.Columns[3].Visible = false;
+            //dataGridView1.Columns[5].Visible = false;
+            //dataGridView1.Columns[7].Visible = false;
+            //dataGridView1.Columns[8].Visible = false;
 
 
         }
         private void button1_Click(object sender, EventArgs e)
         {
             string tenlistnhac = textBox2.Text;
-            int danhsachid = lg.get_danhsachid();//
+            int danhsachid = lg.get_danhsachid();
+          
             lg.add_danhsachchoinhac(danhsachid,tenlistnhac,maid);
 
             /*tao danh sach chi tiet danh sach bai hat*/
@@ -53,7 +57,7 @@ namespace PROJECT.GUI.NGUOIDUNG
                 {
                     if((bool)dgv_row.Cells[0].Value == true)
                     {
-                        int idbaihat = int.Parse(dgv_row.Cells[1].Value.ToString());
+                        int idbaihat = int.Parse(dgv_row.Cells["BAIHATID"].Value.ToString());
 
                         lg.add_chitietdanhsach(danhsachid, idbaihat);
                     }
